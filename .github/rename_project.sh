dae0c5308e71eb15d5228ee124ad8bcafbb6c742
scbrianti2022 <scbrianti2022@users.noreply.github.com>
2022-02-11 17:56:04 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="scbrianti2022"
+original_name="fastapi"
+original_urlname="fastapi"
+original_description="Awesome fastapi created by scbrianti2022"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv fastapi $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
